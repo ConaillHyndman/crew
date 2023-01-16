acfa64ce971bd4c716bb95da5581478840fc6091
Conaill Hyndman <conaillhyndman@Conaills-MacBook-Pro.local>
2023-01-16 17:52:56 +0000

    Router setup

---
+++ src/Home.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+    render() {
+        return (
+            <div>
+                <h1>main header</h1>
+                <p>paragraph</p>
+            </div>
+        );
+    }
+}
+
+export default Home;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Home from './Home';
+import Styles from './Styles';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route exact path='/' element={<Home />} />
+      <Route index element={<Home />} />
+      <Route path='/styles' element={<Styles />} />
+    </Routes>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
