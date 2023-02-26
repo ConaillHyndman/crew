e453ff8c1569c1340f31954fec64a598b8f96466
Conaill Hyndman <conaillhyndman@Conaills-MacBook-Pro.local>
2023-02-26 16:40:00 +0000

    Fixing homepage

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Header from '../Components/Header';
 import Footer from '../Components/Footer';
 import PrimaryButton from '../Components/PrimaryButton';
+import "./Home.css"
 
 const onCrewListButtonClick = () => {
     const link = "/crew-lists";
@@ -17,17 +18,27 @@ class Home extends Component {
     render() {
         return (
             <>
-            <Header />
+                <Header />
                 <div className='home-content-container'>
-                    <h1 className='h2'>main header</h1>
-                    <PrimaryButton
-                    text="Crews"
-                    onClick={onCrewListButtonClick}
-                    />
-                    <PrimaryButton
-                    text="My Club"
-                    onClick={onMyClubButtonClick}
-                    />
+                    <h1 className='h2'>Home</h1>
+                    <div className='cards-container'>
+                        <div className="homepage-cards">
+                            <p className='h3 card-header'>Crews</p>
+                            <p>View the suggested crew lists from your clubs data!</p>
+                            <PrimaryButton
+                                text="View crew lists"
+                                onClick={onCrewListButtonClick}
+                            />
+                        </div>
+                        <div className="homepage-cards">
+                            <p className='h3 card-header'>My club</p>
+                            <p>View your club information and it's members</p>
+                            <PrimaryButton
+                                text="View my club"
+                                onClick={onMyClubButtonClick}
+                            />
+                        </div>
+                    </div>
                 </div>
                 <Footer />
             </>
