1c550c11c28bff61eb7602211891f2b9af6c6437
Conaill Hyndman <conaillhyndman@Conaills-MacBook-Pro.local>
2023-04-17 20:39:02 +0100

    changing npm install to npm ci

--- src/Pages/App.test.js
+++ src/Pages/App.test.js
@@ -1,8 +1,9 @@
-// import { render, screen } from '@testing-library/react';
-// import App from './App';
+import { render, screen } from '@testing-library/react';
+import App from './App';
+import Home from './Home';
 
-// test('renders learn react link', () => {
-//   render(<App />);
-//   const linkElement = screen.getByText(/learn react/i);
-//   expect(linkElement).toBeInTheDocument();
-// });
+test('renders learn react link', () => {
+  render(<Home />);
+  const linkElement = screen.getByText(/View the suggested crew lists/i);
+  expect(linkElement).toBeInTheDocument();
+});
