9a3828a7c0bb4c0b870b37ab3b87c8b352430a24
Conaill Hyndman <conaillhyndman@Conaills-MacBook-Pro.local>
2023-05-03 11:55:12 +0100

    adding unit testing

--- src/Pages/CrewLists.test.js
+++ src/Pages/CrewLists.test.js
@@ -1,15 +1,25 @@
 import { render, screen } from '@testing-library/react';
-import MyClub from './MyClub';
+import CrewLists from './CrewLists';
 import clubMemberData from '../Assets/content/club.json'
 
-test('dynamically renders club data', () => {
-    render(<MyClub />);
-    if (window.location.pathname === "/club-members/carnlough") {
-        const headingContent = clubMemberData.clubs[0].carnlough[0].clubName;
-        expect(headingContent).toBeInTheDocument();
-    } else if (window.location.pathname === "/club-members/cairndhu") {
-        const secondHeadingContent = clubMemberData.clubs[0].cairndhu[0].clubName;
-        console.log(secondHeadingContent);
-        expect(secondHeadingContent).toBeInTheDocument();
+describe('My club test suite', () => {
+
+    function renderList() {
+        if (window.location.pathname === "/club-members/carnlough") {
+            return clubMemberData.clubs[0].carnlough[0].member;
+        } else {
+            return clubMemberData.clubs[0].cairndhu[0].member;
+        }
     }
+
+    it('dynamically renders member data', () => {
+        render(<CrewLists />);
+        if (window.location.pathname === "/club-members/carnlough") {
+            const renderedContent = screen.getByText(renderList()[0].memberName);
+            expect(renderedContent).toBeInTheDocument();
+        } else if (window.location.pathname === "/club-members/carindhu") {
+            const newRenderedContent = screen.getByText(renderList()[0].memberName);
+            expect(newRenderedContent).toBeInTheDocument();
+        }
+    });
 });
--- src/Pages/MyClub.test.js
+++ src/Pages/MyClub.test.js
@@ -3,40 +3,34 @@ import MyClub from './MyClub';
 import clubMemberData from '../Assets/content/club.json'
 
 describe('My club test suite', () => {
-    it('Should render on page', () => {
+
+    function renderList() {
+        if (window.location.pathname === "/club-members/carnlough") {
+            return clubMemberData.clubs[0].carnlough[0].member;
+        } else {
+            return clubMemberData.clubs[0].cairndhu[0].member;
+        }
+    }
+
+    it('Should dynamically render the header', () => {
         render(<MyClub />);
-        const pageHeader = screen.getByText(/Rowing Club/i);
-        expect(pageHeader).toBeInTheDocument();
+        if (window.location.pathname === "/club-members/carnlough") {
+            const carnloughHeading = screen.getByText(clubMemberData.clubs[0].carnlough[0].clubName);
+            expect(carnloughHeading).toBeInTheDocument();
+        } else if (window.location.pathname === "/club-members/cairndhu") {
+            const cairndhuHeading = screen.getByText(clubMemberData.clubs[0].cairndhu[0].clubName);
+            expect(cairndhuHeading).toBeInTheDocument();
+        }
     });
-  });
 
-// function renderList() {
-//     if (window.location.pathname === "/club-members/carnlough") {
-//         return clubMemberData.clubs[0].carnlough[0].member
-//     } else {
-//         return clubMemberData.clubs[0].cairndhu[0].member
-//     }
-// }
-
-// test('dynamically renders club data', () => {
-//     render(<MyClub />);
-//     // if (window.location.pathname === "/club-members/carnlough") {
-//         const headingContent = screen.getByText(clubMemberData.clubs[0].carnlough[0].clubName);
-//         expect(headingContent).toBeInTheDocument();
-//     // } else if (window.location.pathname === "/club-members/cairndhu") {
-//     //     const secondHeadingContent = clubMemberData.clubs[0].cairndhu[0].clubName;
-//     //     console.log(secondHeadingContent);
-//     //     expect(secondHeadingContent).toBeInTheDocument();
-//     // }
-// });
-
-// test('dynamically renders member data', () => {
-//     render(<MyClub />);
-//     if (window.location.pathname === "/club-members/carnlough") {
-//         const renderedContent = renderList()[0].memberName
-//         expect(renderedContent).toBeInTheDocument();
-//     } else if (window.location.pathname === "/club-members/carindhu") {
-//         const newRenderedContent = renderList()[0].memberName
-//         expect(newRenderedContent).toBeInTheDocument();
-//     }
-// });
+    it('dynamically renders member data', () => {
+        render(<MyClub />);
+        if (window.location.pathname === "/club-members/carnlough") {
+            const renderedContent = screen.getByText(renderList()[0].memberName);
+            expect(renderedContent).toBeInTheDocument();
+        } else if (window.location.pathname === "/club-members/carindhu") {
+            const newRenderedContent = screen.getByText(renderList()[0].memberName);
+            expect(newRenderedContent).toBeInTheDocument();
+        }
+    });
+});
